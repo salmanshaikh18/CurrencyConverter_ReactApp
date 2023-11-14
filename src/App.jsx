d2062b074e3f7868fb56cd7e18d6c0c2fc684490
Salman <salmanshaikh18786@gmail.com>
2023-11-14 20:44:32 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl bg-green-500 m-5 rounded text-center p-2 text-white">Currency Converter</h1>
+    </>
+  )
+}
+
+export default App
